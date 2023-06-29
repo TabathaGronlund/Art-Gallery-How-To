292699bfc9c492598c548b40cdcb2b2dd2f9f037
TabathaGronlund <tabathagronlund21@gmail.com>
2023-06-28 20:39:11 -0500

    engaged oil paintings

--- client/src/components/Medias.jsx
+++ client/src/components/Medias.jsx
@@ -18,12 +18,30 @@ const Medias = (props) => {
 
   return (
     <div className='how-to-container'>
+
       <button onClick={() => navigate(`/`)}>home</button>
 
+      <div className='flex-img-oil'>
+        <h1>Famous Oil paintings</h1>
+        <div><p>Mona Lisa</p><img src="/art-img/mona-lisa.png" alt="Mona Lisa" /></div>
+        <div><p>The Girl With The Pearl Earrings</p><img src="/art-img/girl-with-pearl-earrings.png" alt="girl with pearl earrings" /></div>
+        <div><p>The Kiss</p><img src="/art-img/the-kiss.png" alt="the kiss" /></div>
+        <div><p>Starry Night</p><img src="/art-img/starry-night.png" alt="starry night" /></div>
+      </div>
+      <div className='flex-img-acrylic'>
+      <img src="" alt=""/>
+      <img src="" alt=""/>
+      <img src="" alt=""/>
+      <img src="" alt=""/>
+      </div>
+      <div className='flex-img-graphite'>
+      <img src="" alt=""/>
+      <img src="" alt=""/>
+      <img src="" alt=""/>
+      <img src="" alt=""/>
+      </div>
 
-
-
-            <Link to={`/CommentForm`}><button>Add New Comment</button></Link>
+      <Link to={`/CommentForm`}><button>Add New Comment</button></Link>
       {
         allComments.map((comment) => (
           <tr key={comment._id}>
